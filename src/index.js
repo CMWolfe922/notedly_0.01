8ed61704cae7093cde45d6ee5b32273f56108c9e
CMWolfe <cmwolfe1123@gmail.com>
2022-09-23 11:06:50 -0500

    The apollo-server-express package has a bug so I have to use the version 2 in order for the app to work. They are working on fixing it.

--- src/index.js
+++ src/index.js
@@ -1,67 +1,96 @@
-// index.js
-// This is the main entry point of our application
-
-//  ================================================================================== //
 const express = require('express');
-// I need to turn the express server into a GraphQL server using the apollo-server-express
-// package.
 const { ApolloServer, gql } = require('apollo-server-express');
-
-//  ================================================================================== //
-// currently the app is listening on port 4000 which is fine for development. but
-// I will need to make some flexibility to set this to a different port number
-// when deploying the application. To start I will create a `port` variable:
-
+// ================================================================== //
+// Setup the port assignment:
+// ================================================================= //
 const port = process.env.PORT || 4000;
 
-//  ================================================================================== //
-/* Now That I have imported the apollo-server I can set up a basic GraphQL application
-GraphQL applications consist of two primary components: a schema of type definitions
-and resolvers., which resolve the queries and mutations performed against the data. */
+// ================================================================= //
+// Create Notes:
+// ================================================================= //
+let notes = [
+    {
+        id: '1',
+        content: 'This is a note',
+        author: 'Adam Scott'
+    },
+    {
+        id: '2',
+        content: 'This is another note',
+        author: 'Harlow Everly'
+    },
+    {
+        id: '3',
+        content: 'Oh hey look, another note!',
+        author: 'Riley Harrison'
+    }
+];
 
-// So to begin I will build a basic schema:
-const typeDefs = gql
-    `type Query {
+// =============================================================== //
+// Create a Schema using GraphQL Schema language:
+// =============================================================== //
+const typeDefs = gql`
+  type Note {
+    id: ID
+    content: String
+    author: String
+  }
+
+  type Query {
     hello: String
-    }`;
+    notes: [Note]
+    note(id: ID): Note
+  }
 
-// Create the Resolver functions for the schema
+  type Mutation {
+    newNote(content: String!): Note
+  }
+`;
+
+// =============================================================== //
+// Create a resolver function for the schema language
+// =============================================================== //
 const resolvers = {
     Query: {
-        hello: () => "Hello World!"
+        hello: () => 'Hello world!',
+        notes: () => notes,
+        note: (parent, args) => {
+            return notes.find(note => note.id === args.id);
+        }
+    },
+    Mutation: {
+        newNote: (parent, args) => {
+            let noteValue = {
+                id: String(notes.length + 1),
+                content: args.content,
+                author: 'Adam Scott'
+            };
+            notes.push(noteValue);
+            return noteValue;
+        }
     }
 };
 
-//  ================================================================================== //
+// ============================================================== //
+// Create the app assignment:
+// ============================================================== //
 const app = express();
 
-//  ================================================================================== //
-// Next I will integrate Apollo Server to serve GraphQL API. To Do so, we'll add
-// some Apollo Server specific settings and middleware and update the app.listen
-// method:
-
-// Apollo Server Setup:
+// ============================================================== //
+// Build the new Apollo Server:
+// ============================================================== //
 const server = new ApolloServer({ typeDefs, resolvers });
 
-//  ================================================================================== //
-app.get('/', (req, res) => res.send('Hello World!!'));
-
-
-//  ================================================================================== //
+// ============================================================== //
 // Apply the Apollo GraphQL middleware and set the path to /api
+// ============================================================== //
 server.applyMiddleware({ app, path: '/api' });
 
-//  ================================================================================== //
-// This change will allow me to dynamically set the port in the Node environment,
-// but fall back to port 4000 when no port is specified. Now I will have to
-// adjust the app.listen() method to work with this change and use a template
-//  literal to log the correct port.
-
-// Update this to accomodate the GraphQL API:
-app.listen(port, () =>
+// ============================================================== //
+// Build the app.listen  method:
+// ============================================================== //
+app.listen({ port }, () =>
     console.log(
-        'GraphQL Server running at http://localhost:${port}${server.graphqlPath}'
+        `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
     )
 );
-
-//  ================================================================================== //

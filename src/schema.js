7b40c3bf62976bd06aa694fc4fad7f18f1e37462
CMWolfe <cmwolfe1123@gmail.com>
2022-09-23 15:10:10 -0500

    Added scalar DateTime and createdAt and UpdatedAt to the Note model

--- src/schema.js
+++ src/schema.js
@@ -4,10 +4,13 @@ const { gql } = require('apollo-server-express');
 // Construct a schema, using GraphQL's schema language
 // =============================================================== //
 module.exports = gql`
+  scalar DateTime
   type Note {
     id: ID!
     content: String!
     author: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
   }
 
   type Query {

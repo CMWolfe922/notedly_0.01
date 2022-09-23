a8cf372b2f835b5d756d9f7d4203aecb4cafc50a
CMWolfe <cmwolfe1123@gmail.com>
2022-09-23 15:04:48 -0500

    Updated the src/resolvers/mutation file to add CRUD capabilities across the apps API setup.

--- src/resolvers/mutations.js
+++ src/resolvers/mutations.js
@@ -2,10 +2,29 @@ const { models } = require("mongoose");
 
 
 module.exports = {
+
     newNote: async (parent, args, { models }) => {
         return await models.Note.create({
             content: args.content,
             author: 'Charles M. Wolfe'
         });
-    }
+    },
+
+    deleteNote: async (parent, { id }, { models }) => {
+        try {
+            await models.Note.findOneAndRemove({ _id: id });
+            return true;
+        } catch (err) {
+            return false;
+        }
+    },
+
+    updateNote: async (parent, { content, id }, { models }) => {
+        return await models.Note.findOneAndUpdate(
+            { _id: id },
+            { $set: { content } },
+            { new: true }
+        );
+    },
+
 }

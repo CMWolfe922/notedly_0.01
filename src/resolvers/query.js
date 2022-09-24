8a86d29dc61985b0b31ba4de6ce959ac99e537a7
CMWolfe <cmwolfe1123@gmail.com>
2022-09-24 17:04:07 -0500

    Updated the query.js script

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,10 +1,27 @@
 const { models } = require("mongoose")
 
 module.exports = {
+
     notes: async (parent, args, { models }) => {
         return await models.Note.find()
     },
+
     note: async (parent, args, { models }) => {
         return await models.Note.findById(args.id);
+    },
+
+    user: async (parent, { username }, { models }) => {
+        // find a user given their username
+        return await models.User.findOne({ username });
+    },
+
+    users: async (parent, args, { models }) => {
+        //find all users
+        return await models.User.find({});
+    },
+
+    me: async (parent, args, { models, user }) => {
+        // find a user given their current user context
+        return await models.USer.findById(user.id);
     }
 }

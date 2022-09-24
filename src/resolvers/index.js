6387a7e44687f5dffe51944e21c0a8f2accc7674
CMWolfe <cmwolfe1123@gmail.com>
2022-09-24 17:48:15 -0500

    Updated the resolver index.js file to add the note and user scripts

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,10 +1,14 @@
 const Query = require('./query');
 const Mutation = require('./mutations');
+const Note = require('./note');
+const User = require('./user');
 const { GraphQLDateTime } = require('graphql-iso-date');
 
 
 module.exports = {
     Query,
     Mutation,
+    Note,
+    User,
     DateTime: GraphQLDateTime
 };
